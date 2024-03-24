49b3e0747bfe9b01c4b6b005e4b1ebad4e4e82fb
RGlimadigital <RgFullDev@gmail.com>
2024-03-24 17:11:46 -0300

    Conectando o com o Banco de dados

--- src/main/java/com/example/demo/resources/UserResource.java
+++ src/main/java/com/example/demo/resources/UserResource.java
@@ -1,32 +1,26 @@
 package com.example.demo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.User;
+import com.example.demo.services.UserService;
 
 @RestController
 @RequestMapping(value  = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll(){
-		User maria = new User("1", "Maria", "maria@gmail.com");
-		User jose = new User("2", "Jose", "jose@gmail.com");
-		User joao = new User("3", "Joao", "joao@gmail.com");
-		
-		List<User> usuarios = new ArrayList<>();
-		
-		usuarios.addAll(Arrays.asList(maria, jose, joao));
-		
-		return ResponseEntity.ok().body(usuarios);
+		return ResponseEntity.ok().body(service.findAll());
 	}
 
 }
